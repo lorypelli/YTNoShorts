e754c0a998aebdeb75bbe2337f1072e53a80f590
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-09-13 10:06:17 +0200

    fixed a bug and updated packages

--- content.ts
+++ content.ts
@@ -1,33 +1,29 @@
-import type { PlasmoCSConfig } from 'plasmo';
 import { Storage } from '@plasmohq/storage';
 const storage = new Storage();
-export const config: PlasmoCSConfig = {
-    matches: [
-        'https://www.youtube.com/shorts/*'
-    ]
-};
 const checked = storage.get('checked');
-const url = window.location.href;
-if (url && url.startsWith('https://www.youtube.com/')) {
-    if (url.includes('/shorts')) {
-        Promise.resolve(checked).then(async c => {
-            if (c == null) {
-                await storage.set('checked', '0');
-                c = '0';
-            }
-            if (await storage.get('shortcut') == null) {
-                await storage.set('shortcut', 'ALT + Q');
-            }
-            if (c == '1') {
-                window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-            }
-            else if (c == '0') {
-                window.addEventListener('keydown', async e => {
-                    if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
-                        window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-                    }
-                });
-            }
-        });
+setInterval(() => {
+    const url = window.location.href;
+    if (url && url.startsWith('https://www.youtube.com/')) {
+        if (url.includes('/shorts')) {
+            Promise.resolve(checked).then(async c => {
+                if (c == null) {
+                    await storage.set('checked', '0');
+                    c = '0';
+                }
+                if (await storage.get('shortcut') == null) {
+                    await storage.set('shortcut', 'ALT + Q');
+                }
+                if (c == '1') {
+                    window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+                }
+                else if (c == '0') {
+                    window.addEventListener('keydown', async e => {
+                        if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
+                            window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+                        }
+                    });
+                }
+            });
+        }
     }
-}
+}, 1000);
