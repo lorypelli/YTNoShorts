ad2dc80eeb50a808a352dc404e01c2cccd46daea
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-09-16 22:18:10 +0200

    reload not necessary while setting always replace

--- content.ts
+++ content.ts
@@ -1,35 +1,32 @@
 import type { PlasmoCSConfig } from 'plasmo';
 import { Storage } from '@plasmohq/storage';
 const storage = new Storage();
-const checked = storage.get('checked');
 export const config: PlasmoCSConfig = {
     matches: [
         'https://www.youtube.com/*'
     ]
 };
-setInterval(() => {
+setInterval(async () => {
     const url = window.location.href;
     if (url && url.startsWith('https://www.youtube.com/')) {
         if (url.includes('/shorts')) {
-            Promise.resolve(checked).then(async c => {
-                if (c == null) {
-                    await storage.set('checked', '0');
-                    c = '0';
-                }
-                if (await storage.get('shortcut') == null) {
-                    await storage.set('shortcut', 'ALT + Q');
-                }
-                if (c == '1') {
-                    window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-                }
-                else if (c == '0') {
-                    window.addEventListener('keydown', async e => {
-                        if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
-                            window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-                        }
-                    });
-                }
-            });
+            const checked = await storage.get('checked');
+            if (checked == null) {
+                await storage.set('checked', '0');
+            }
+            if (await storage.get('shortcut') == null) {
+                await storage.set('shortcut', 'ALT + Q');
+            }
+            if (checked == '1') {
+                window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+            }
+            else if (checked == '0') {
+                window.addEventListener('keydown', async e => {
+                    if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
+                        window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+                    }
+                });
+            }
         }
     }
 }, 1000);
