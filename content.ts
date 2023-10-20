7fb7ac80c3b3dca32801ec552fd1e8a0d85b5b9c
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-10-20 14:29:06 +0200

    finished disable extension option

--- content.ts
+++ content.ts
@@ -11,28 +11,26 @@ setInterval(async () => {
     if (url && url.startsWith('https://www.youtube.com/')) {
         if (url.includes('/shorts')) {
             const checked = await storage.get('checked');
+            const shortcut = await storage.get('shortcut');
             const extension = await storage.get('extension');
             if (extension == null) {
                 await storage.set('extension', '0');
             }
-            if (extension == '1') return;
-            else {
-                if (checked == null) {
-                    await storage.set('checked', '0');
-                }
-                if (await storage.get('shortcut') == null) {
-                    await storage.set('shortcut', 'ALT + Q');
-                }
-                if (checked == '1') {
-                    window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-                }
-                else if (checked == '0') {
-                    window.addEventListener('keydown', async e => {
-                        if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
-                            window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
-                        }
-                    });
-                }
+            if (checked == null) {
+                await storage.set('checked', '0');
+            }
+            if (shortcut == null) {
+                await storage.set('shortcut', 'ALT + Q');
+            }
+            if (checked == '1' && extension == '0') {
+                window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+            }
+            else if (checked == '0' && extension == '0') {
+                window.addEventListener('keydown', async e => {
+                    if (e.altKey && e.key.toUpperCase() == (await storage.get('shortcut')).split('+')[1].trim()) {
+                        window.location.href = `https://www.youtube.com/watch?v=${url.split('/')[4]}`;
+                    }
+                });
             }
         }
     }
