97fa4d6c27eb548446d27b6c856a2a4ef4e0e726
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-18 15:22:54 +0100

    fix popup disable element

--- components/Disable.tsx
+++ components/Disable.tsx
@@ -6,15 +6,15 @@ export default function Disable() {
     const [extDisabled, setExtDisabled] = useState(false);
     const [extension, setExtension] = useStorage('extension', true);
     useEffect(() => {
-        setExtDisabled(extension);
+        setExtDisabled(!extension);
     }, [extension]);
     return (
         <>
-            <h1>Extension is currently {!extDisabled ? 'disabled' : 'enabled'}</h1>
+            <h1>Extension is currently {extDisabled ? 'disabled' : 'enabled'}</h1>
             <Button id='disable' onClick={async () => {
                 setExtension(!extDisabled);
                 setExtDisabled(!extDisabled);
-            }}>{extDisabled ? 'DISABLE' : 'ENABLE'}</Button>
+            }}>{!extDisabled ? 'DISABLE' : 'ENABLE'}</Button>
         </>
     );
 }
